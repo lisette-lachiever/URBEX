92717d83a0224c053debb09b58d83728c55dbafc
Arjuna <c.ndubuaku@alustudent>
2026-04-17 13:12:56 +0400

    Added nport config for railway

--- backend/app.py
+++ backend/app.py
@@ -830,4 +830,5 @@ def static_files(filename):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, threaded=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, threaded=True)
